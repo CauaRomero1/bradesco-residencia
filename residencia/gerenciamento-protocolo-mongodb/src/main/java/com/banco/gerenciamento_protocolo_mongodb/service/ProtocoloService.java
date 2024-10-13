327f6e6d7f9a84afaa6d5760b91a8231c5ff1c4f
vinicius <viniciussantana2003@hotmail.com>
2024-10-13 18:09:38 -0300

    Implementação de funções: Mudança de Responsável

--- residencia/gerenciamento-protocolo-mongodb/src/main/java/com/banco/gerenciamento_protocolo_mongodb/service/ProtocoloService.java
+++ residencia/gerenciamento-protocolo-mongodb/src/main/java/com/banco/gerenciamento_protocolo_mongodb/service/ProtocoloService.java
@@ -1,6 +1,8 @@
 package com.banco.gerenciamento_protocolo_mongodb.service;
 
+import com.banco.gerenciamento_protocolo_mongodb.model.Funcionario;
 import com.banco.gerenciamento_protocolo_mongodb.model.Protocolo;
+import com.banco.gerenciamento_protocolo_mongodb.repository.FuncionarioRepository;
 import com.banco.gerenciamento_protocolo_mongodb.repository.ProtocoloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,9 @@ public class ProtocoloService {
 
     @Autowired
     private ProtocoloRepository protocoloRepository;
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;  // Injeção do repositório de Funcionário
 
     public Protocolo criarProtocolo(Protocolo protocolo) {
         protocolo.setNumeroProtocolo(UUID.randomUUID().toString());
@@ -42,9 +47,32 @@ public class ProtocoloService {
         throw new RuntimeException("Protocolo não encontrado");
     }
 
+    public Protocolo mudarResponsavel(String numeroProtocolo, String codigoFuncionarioNovo) {
+        Optional<Protocolo> optionalProtocolo = protocoloRepository.findByNumeroProtocolo(numeroProtocolo);
+        if (optionalProtocolo.isPresent()) {
+            Protocolo protocolo = optionalProtocolo.get();
+
+            // Buscar o novo funcionário
+            Optional<Funcionario> optionalFuncionario = funcionarioRepository.findById(codigoFuncionarioNovo);
+            if (optionalFuncionario.isPresent()) {
+                Funcionario novoFuncionario = optionalFuncionario.get();
+
+                // Atualiza o responsável (funcionário)
+                protocolo.setFuncionario(novoFuncionario);
+                protocolo.setDataAlteracao(LocalDate.now());  // Atualiza a data de alteração
+
+                // Salva o protocolo com o novo responsável
+                return protocoloRepository.save(protocolo);
+            } else {
+                throw new RuntimeException("Funcionário não encontrado");
+            }
+        } else {
+            throw new RuntimeException("Protocolo não encontrado");
+        }
+    }
+
     private LocalDate calcularPrazo(String tipoProtocolo) {
         // Implementação do cálculo de prazo aqui
         return null;
     }
 }
-
